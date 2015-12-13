6cb0a3b4017c914440bd6bc278bb79a512c2472a
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-12-13 23:20:47 +0000

    Making it possible for late static binding of properties

--- src/ConfigManager.php
+++ src/ConfigManager.php
@@ -12,21 +12,21 @@ class ConfigManager
     
     public static function init($path)
     {
-        self::$data = new Data($path);
+        static::$data = new Data($path);
     }
     
     public static function get($key, $default = null)
     {
-        return self::$data->isKeySet($key) ? self::$data->get($key) : $default;
+        return static::$data->isKeySet($key) ? static::$data->get($key) : $default;
     }
     
     public static function set($key, $value)
     {
-        return self::$data->set($key, $value);
+        return static::$data->set($key, $value);
     }
     
     public static function setContext($context)
     {
-        self::$data->setContext($context);
+        static::$data->setContext($context);
     }
 }
